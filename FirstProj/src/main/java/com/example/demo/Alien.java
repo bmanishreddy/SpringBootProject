6edafdda2d58e23a2c45d8e9f10024994622613c
Manish Reddy Bendhi <bmanishreddy@hotmail.com>
2020-08-09 18:49:26 -0500

    adding aliasobject

--- FirstProj/src/main/java/com/example/demo/Alien.java
+++ FirstProj/src/main/java/com/example/demo/Alien.java
@@ -1,11 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component 
+@Scope(value="prototype")
 public class Alien {
 
 	private int aid;
 	private String aname;
 	private String tech;
-	public int getAid() {
+	@Autowired
+	private Laptop laptop;
+		public int getAid() {
 		return aid;
 	}
 	public void setAid(int aid) {
@@ -22,6 +30,17 @@ public class Alien {
 	}
 	public void setTech(String tech) {
 		this.tech = tech;
+	}
+	public Laptop getLaptop() {
+		return laptop;
+	}
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+	public void show()
+	{
+		
+		System.out.println("in show");
 	}
 	
 	
--- FirstProj/src/main/java/com/example/demo/FirstProjApplication.java
+++ FirstProj/src/main/java/com/example/demo/FirstProjApplication.java
@@ -9,6 +9,9 @@ public class FirstProjApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstProjApplication.class, args);
 		System.out.println("Firs spring ...");
+		
+		//This is the normal way
+		//Alien a = new Alien();
 	}
 
 }
